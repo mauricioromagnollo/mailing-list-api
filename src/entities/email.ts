856c3457743cacd66cf08cb8153418f6f2dccadb
x0n4d0 <romagnolimauricio@gmail.com>
2022-08-07 17:18:37 -0300

    feat: validate emails with domain larger than 255

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,12 +1,24 @@
 export class Email {
+  public static readonly MAX_EMAIL_SIZE = 320;
+  public static readonly MAX_EMAIL_LOCALPART_SIZE = 64;
+  public static readonly MAX_EMAIL_DOMAIN_SIZE = 255;
+
   static validate(email: string): boolean {
     if (!email) {
       return false;
     }
 
-    const [localpart] = email.split('@');
+    if (email.length > this.MAX_EMAIL_SIZE) {
+      return false;
+    }
 
-    if (localpart.length > 64) {
+    const [localpart, domain] = email.split('@');
+
+    if (localpart.length > this.MAX_EMAIL_LOCALPART_SIZE) {
+      return false;
+    }
+
+    if (domain.length > this.MAX_EMAIL_DOMAIN_SIZE) {
       return false;
     }
 
