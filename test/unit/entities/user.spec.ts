21f2718c75ed703ca566443c7191793f063d6614
x0n4d0 <romagnolimauricio@gmail.com>
2022-08-07 22:48:33 -0300

    test: validate atributes and create valid user

--- test/unit/entities/user.spec.ts
+++ test/unit/entities/user.spec.ts
@@ -1,6 +1,8 @@
 import { describe, test } from '@/test/ports';
 
-import { User, InvalidEmailError } from '@/entities';
+import {
+  User, InvalidEmailError, InvalidNameError,
+} from '@/entities';
 import { left } from '@/shared';
 
 describe('User (Domain Entity)', () => {
@@ -8,5 +10,25 @@ describe('User (Domain Entity)', () => {
     const invalidEmail = 'invalid_email';
     const error = User.create({ name: 'any_name', email: invalidEmail });
     expect(error).toEqual(left(new InvalidEmailError()));
+  });
+
+  test('should not create user with invalid name (too few characteres)', () => {
+    const invalidName = 'O      ';
+    const error = User.create({ name: invalidName, email: 'any@email.com' });
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  test('should not create user with invalid name (too many characteres)', () => {
+    const invalidName = 'O'.repeat(257);
+    const error = User.create({ name: invalidName, email: 'any@email.com' });
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  test('should create user with valid data', () => {
+    const validName = 'valid_name';
+    const validEmail = 'any@mail.com';
+    const user: User = User.create({ name: validName, email: validEmail }).value as User;
+    expect(user.name.value).toEqual(validName);
+    expect(user.email.value).toEqual(validEmail);
   });
 });
