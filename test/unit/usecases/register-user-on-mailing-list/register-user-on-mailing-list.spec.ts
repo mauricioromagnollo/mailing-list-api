3100a28a80c582a2b553e3a77f55b0f3020407eb
x0n4d0 <romagnolimauricio@gmail.com>
2022-08-13 00:35:14 -0300

    refactor: move InMemoryUserRepository to test doubles layer

--- test/unit/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/unit/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -2,7 +2,7 @@ import { describe, test, expect } from '@/test/ports';
 import { UserRepository } from '@/usecases';
 import { UserData } from '@/entities';
 
-import { InMemoryUserRepository } from './in-memory-user-repository';
+import { InMemoryUserRepository } from '@/test/doubles/fakes';
 
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
