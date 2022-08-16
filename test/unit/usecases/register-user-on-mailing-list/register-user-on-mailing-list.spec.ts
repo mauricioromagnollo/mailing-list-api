15ffcb49b62086cf754b99f70e891a9f36181979
x0n4d0 <romagnolimauricio@gmail.com>
2022-08-15 21:43:33 -0300

    test: register valid user to mailing list

--- test/unit/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/unit/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from '@/test/ports';
-import { UserRepository } from '@/usecases';
+import { UserRepository, RegisterUserOnMailingList } from '@/usecases';
 import { UserData } from '@/entities';
 
 import { InMemoryUserRepository } from '@/test/doubles/fakes';
@@ -8,6 +8,12 @@ describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = [];
     const userRepository: UserRepository = new InMemoryUserRepository(users);
-    expect(userRepository).not.toBeNull();
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(userRepository);
+    const name = 'any_name';
+    const email = 'any@email.com';
+    const response = await usecase.registerUserOnMailingList({ name, email });
+    const user = await userRepository.findUserByEmail(email);
+    expect(user.name).toBe(name);
+    expect(response.value.name).toBe(name);
   });
 });
