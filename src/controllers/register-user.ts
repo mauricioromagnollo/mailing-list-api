797165099d67c9ee74c179270b4bbb8ea74728e7
x0n4d0 <romagnolimauricio@gmail.com>
2022-09-01 01:55:21 -0300

    feat: should return 500 when internal server error

--- src/controllers/register-user.ts
+++ src/controllers/register-user.ts
@@ -1,40 +1,44 @@
+import { UseCase } from '@/usecases/ports';
 import { MissingParamError } from '@/controllers/errors';
 import { HttpRequest, HttpResponse } from '@/controllers/ports';
 import { UserData } from '@/entities';
-import { RegisterUserOnMailingList } from '@/usecases';
-import { created, badRequest } from '@/controllers/helpers';
+import { created, badRequest, internalServerError } from '@/controllers/helpers';
 
 export class RegisterUserController {
-  private readonly usecase: RegisterUserOnMailingList;
+  private readonly registerUserOnMailingList: UseCase;
 
-  constructor(usecase: RegisterUserOnMailingList) {
-    this.usecase = usecase;
+  constructor(usecase: UseCase) {
+    this.registerUserOnMailingList = usecase;
   }
 
   public async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { name, email } = request.body;
+    try {
+      const { name, email } = request.body;
 
-    if (!name && !email) {
-      return badRequest(new MissingParamError('name email'));
+      if (!name && !email) {
+        return badRequest(new MissingParamError('name email'));
+      }
+
+      if (!name || !email) {
+        const missingParam = !name ? 'name' : 'email';
+        return badRequest(new MissingParamError(missingParam));
+      }
+
+      const userData: UserData = request.body;
+      const registerUserOnMailingList = await this.registerUserOnMailingList.perform(userData);
+      let httpResponse: HttpResponse;
+
+      if (registerUserOnMailingList.isLeft()) {
+        httpResponse = badRequest(registerUserOnMailingList.value);
+      }
+
+      if (registerUserOnMailingList.isRight()) {
+        httpResponse = created(registerUserOnMailingList.value);
+      }
+
+      return httpResponse;
+    } catch (error) {
+      return internalServerError(error);
     }
-
-    if (!name || !email) {
-      const missingParam = !name ? 'name' : 'email';
-      return badRequest(new MissingParamError(missingParam));
-    }
-
-    const userData: UserData = request.body;
-    const registerUserOnMailingList = await this.usecase.registerUserOnMailingList(userData);
-    let httpResponse: HttpResponse;
-
-    if (registerUserOnMailingList.isLeft()) {
-      httpResponse = badRequest(registerUserOnMailingList.value);
-    }
-
-    if (registerUserOnMailingList.isRight()) {
-      httpResponse = created(registerUserOnMailingList.value);
-    }
-
-    return httpResponse;
   }
 }
