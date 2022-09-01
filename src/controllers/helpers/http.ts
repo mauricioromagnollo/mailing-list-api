83d19d0227258427b6a7c5f317be7c5d9a666f01
x0n4d0 <romagnolimauricio@gmail.com>
2022-09-01 01:49:22 -0300

    feat: add internalServerError and status

--- src/controllers/helpers/http.ts
+++ src/controllers/helpers/http.ts
@@ -3,6 +3,7 @@ import { HttpResponse } from '@/controllers/ports';
 export const HTTP_STATUS_CODE = {
   CREATED: 201,
   BAD_REQUEST: 400,
+  INTERNAL_SERVER_ERROR: 500,
 };
 
 export const created = (data: any): HttpResponse => ({
@@ -12,5 +13,10 @@ export const created = (data: any): HttpResponse => ({
 
 export const badRequest = (data: any): HttpResponse => ({
   statusCode: HTTP_STATUS_CODE.BAD_REQUEST,
+  body: data,
+});
+
+export const internalServerError = (data: any): HttpResponse => ({
+  statusCode: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
   body: data,
 });
