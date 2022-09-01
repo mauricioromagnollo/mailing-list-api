7eefe59bb1a0de048ed26ee5422341eb1f08fd34
x0n4d0 <romagnolimauricio@gmail.com>
2022-09-01 00:41:20 -0300

    feat: return 400 when invalid name or email

--- src/controllers/register-user.ts
+++ src/controllers/register-user.ts
@@ -1,7 +1,7 @@
 import { HttpRequest, HttpResponse } from '@/controllers/ports';
 import { UserData } from '@/entities';
 import { RegisterUserOnMailingList } from '@/usecases';
-import { created } from '@/controllers/helpers';
+import { created, badRequest } from '@/controllers/helpers';
 
 export class RegisterUserController {
   private readonly usecase: RegisterUserOnMailingList;
@@ -12,11 +12,15 @@ export class RegisterUserController {
 
   public async handle(request: HttpRequest): Promise<HttpResponse> {
     const userData: UserData = request.body;
-    const response = await this.usecase.registerUserOnMailingList(userData);
+    const registerUserOnMailingList = await this.usecase.registerUserOnMailingList(userData);
     let httpResponse: HttpResponse;
 
-    if (response.isRight()) {
-      return created(response.value);
+    if (registerUserOnMailingList.isLeft()) {
+      httpResponse = badRequest(registerUserOnMailingList.value);
+    }
+
+    if (registerUserOnMailingList.isRight()) {
+      httpResponse = created(registerUserOnMailingList.value);
     }
 
     return httpResponse;
