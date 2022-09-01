765bcaf7d43cd47f7cbd33262adda6c71af4c5fc
x0n4d0 <romagnolimauricio@gmail.com>
2022-09-01 00:41:54 -0300

    test: ensure return status code 400 when invalid name or email on request

--- test/unit/controllers/register-user.spec.ts
+++ test/unit/controllers/register-user.spec.ts
@@ -6,6 +6,7 @@ import { UserRepository } from '@/usecases/ports';
 import { UserData } from '@/entities';
 import { RegisterUserController } from '@/controllers';
 import { HTTP_STATUS_CODE } from '@/controllers/helpers';
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors';
 
 type SutType = RegisterUserController;
 
@@ -33,5 +34,31 @@ describe('Register User Controller', () => {
     const response: HttpResponse = await sut.handle(request);
     expect(response.statusCode).toEqual(HTTP_STATUS_CODE.CREATED);
     expect(response.body).toEqual(request.body);
+  });
+
+  test('should return status code 400 when request contains invalid name', async () => {
+    const sut = makeSut();
+    const requestWithInvalidName: HttpRequest = {
+      body: {
+        name: 'A',
+        email: 'any@mail.com',
+      },
+    };
+    const response: HttpResponse = await sut.handle(requestWithInvalidName);
+    expect(response.statusCode).toEqual(HTTP_STATUS_CODE.BAD_REQUEST);
+    expect(response.body).toBeInstanceOf(InvalidNameError);
+  });
+
+  test('should return status code 400 when request contains invalid name', async () => {
+    const sut = makeSut();
+    const requestWithInvalidEmail: HttpRequest = {
+      body: {
+        name: 'Any Name',
+        email: 'invalid_email.com',
+      },
+    };
+    const response: HttpResponse = await sut.handle(requestWithInvalidEmail);
+    expect(response.statusCode).toEqual(HTTP_STATUS_CODE.BAD_REQUEST);
+    expect(response.body).toBeInstanceOf(InvalidEmailError);
   });
 });
