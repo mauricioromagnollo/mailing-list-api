e84f9149cae952992779f26ced34e875f2a6f96b
x0n4d0 <romagnolimauricio@gmail.com>
2022-09-01 00:39:54 -0300

    feat: add badRequest http response and status code

--- src/controllers/helpers/http.ts
+++ src/controllers/helpers/http.ts
@@ -2,9 +2,15 @@ import { HttpResponse } from '@/controllers/ports';
 
 export const HTTP_STATUS_CODE = {
   CREATED: 201,
+  BAD_REQUEST: 400,
 };
 
 export const created = (data: any): HttpResponse => ({
   statusCode: HTTP_STATUS_CODE.CREATED,
+  body: data,
+});
+
+export const badRequest = (data: any): HttpResponse => ({
+  statusCode: HTTP_STATUS_CODE.BAD_REQUEST,
   body: data,
 });
