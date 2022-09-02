aa129d6a299e0efacddc01f40edee1fc6ab32272
x0n4d0 <romagnolimauricio@gmail.com>
2022-09-01 23:28:27 -0300

    refactor: change way to write type definitions

--- src/controllers/helpers/http.ts
+++ src/controllers/helpers/http.ts
@@ -6,17 +6,17 @@ export const HTTP_STATUS_CODE = {
   INTERNAL_SERVER_ERROR: 500,
 };
 
-export const created = (data: any): HttpResponse => ({
+export const created = <T = any> (data: T): HttpResponse => ({
   statusCode: HTTP_STATUS_CODE.CREATED,
   body: data,
 });
 
-export const badRequest = (data: any): HttpResponse => ({
+export const badRequest = <T = any> (data: T): HttpResponse => ({
   statusCode: HTTP_STATUS_CODE.BAD_REQUEST,
   body: data,
 });
 
-export const internalServerError = (data: any): HttpResponse => ({
+export const internalServerError = <T = any> (data: T): HttpResponse => ({
   statusCode: HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR,
   body: data,
 });
